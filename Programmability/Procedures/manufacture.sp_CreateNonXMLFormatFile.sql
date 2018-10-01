3549ad8b86a7e9ccf2e9e6c9125520dea29834f0
Aleksey <polyatykina@gmail.com>
2018-10-01 09:17:48 +0300

    Проверка комитов

--- Programmability/Procedures/manufacture.sp_CreateNonXMLFormatFile.sql
+++ Programmability/Procedures/manufacture.sp_CreateNonXMLFormatFile.sql
@@ -3,7 +3,7 @@ GO
 /*$Create:     Zapadinskiy Anatoliy$    $Create date:   17.12.2013$*/
 /*$Modify:     Zapadinskiy Anatoliy$    $Modify date:   08.04.2014$*/
 /*$Version:    1.00$   $Description: Создаем файл настроек для bulk insert$*/
-create PROCEDURE [manufacture].[sp_CreateNonXMLFormatFile] 
+CREATE PROCEDURE [manufacture].[sp_CreateNonXMLFormatFile] 
     @ColumnCount tinyint, 
     @Separator varchar(1),
 	@FilePath varchar(512)
@@ -21,7 +21,7 @@ BEGIN
         EXEC master..xp_cmdshell @command, no_output
 
         SET @i = 1
-        SET @command = 'CREATE TABLE #TMCImportFile (ID int IDENTITY(1,1) NOT NULL, '
+--        SET @command = 'CREATE TABLE #TMCImportFile (ID int IDENTITY(1,1) NOT NULL, '
         WHILE @i <= @ColumnCount
         BEGIN    	
             SET @command = Convert(varchar(3),@i) + '       SQLCHAR      0       255     "' + CASE WHEN @i = @ColumnCount THEN '\r\n' ELSE @Separator END + '"   ' + Convert(varchar(3),@i+1) + '     ' +'Column_' + Convert(varchar(3),@i) + '                 ""'
